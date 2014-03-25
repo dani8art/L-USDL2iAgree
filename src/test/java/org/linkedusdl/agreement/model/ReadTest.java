b77d7271d3e673cc98e955894672ed4bc70238ba
Daniel Arteaga <dani8art.da@gmail.com>
2014-03-25 15:22:55 +0100

    no message

--- src/test/java/org/linkedusdl/agreement/model/ReadTest.java
+++ src/test/java/org/linkedusdl/agreement/model/ReadTest.java
@@ -17,30 +17,6 @@ public class ReadTest {
 
 	@Test
 	public void newTest() throws RDFBeanException, ModelRuntimeException, IOException {
-		// Loads RDF2Go model
-		/*RDF2Go.register(new RepositoryModelFactory());
-		ModelFactory modelFactory = RDF2Go.getModelFactory();
-		Model model = modelFactory.createModel();
-		model.open();
-		
-		// Reads amazonEC2.ttl file into RDF2Go model
-		InputStream source = getClass().getResourceAsStream("test.ttl");
-		model.readFrom(source, Syntax.Turtle);
-
-		// Initializes the bean manager
-		RDFBeanManager manager = new RDFBeanManager(model);
-
-		// Gets the service offering that corresponds with the URI :amazonEC2ServiceOfferingM1LargeInstance
-		ServiceOffering so = manager.create("http://purl.org/cloudComputing/amazonEC2#amazonEC2ServiceOfferingM1LargeInstanceType",
-						ServiceOffering.class);
-		printServiceOffering(so);
-
-		// Checks everything is fine
-		Assert.assertEquals("http://purl.org/cloudComputing/amazonEC2#amazonEC2ServiceOfferingM1LargeInstanceType", so.getId());
-		Assert.assertEquals(1, so.getComplyWith().size());
-		Assert.assertEquals("http://purl.org/cloudComputing/amazonEC2#ec2ServiceCommitment", so.getComplyWith().iterator().next().getId());
-		
-		model.close();*/
 		try{
 			USDLModel model = new USDLModel(getClass().getResourceAsStream("test.ttl"));
 
